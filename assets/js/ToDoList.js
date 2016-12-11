18473b9295500ddd1d624ec5eb69f8f38ce6b41d
manuel salomon <manuel salomon>
2016-12-11 20:17:31 -0300

    Retoques finales

--- assets/js/ToDoList.js
+++ assets/js/ToDoList.js
@@ -1,19 +1,27 @@
 /*tachando tareas de la lista*/
-$("ul").on("click", "li", function(){
+$("ul").on("click", "li", function() {
     $(this).toggleClass("completado")
-    });
+});
 /*borrando tareas de la lista*/
-$("ul").on("click", "span", function(event){
-    $(this).parent().fadeOut(500, function(){
-      $(this).remove();
-      });
+$("ul").on("click", "span", function(event) {
+    $(this).parent().fadeOut(500, function() {
+        $(this).remove();
+    });
     event.stopPropagation();
-      });
+});
 /*agregando tareas a la lista*/
-$("input[type='text']").keypress(function(event){
-  if(event.key == "Enter") {
-    $("ul").append("<li><span><i class='fa fa-trash' aria-hidden='true'></i></span> "+ $(this).val() + "</li>");
-    $(this).val("");
-    event.stopPropagation();
+$("input[type='text']").keypress(function(event) {
+    if (event.key == "Enter") {
+        if ($(this).val() == "") {
+            alert("ingrese una tarea")
+        } else {
+            $("ul").append("<li><span><i class='fa fa-trash' aria-hidden='true'></i></span> " + $(this).val() + "</li>");
+            $(this).val("");
+            event.stopPropagation();
+        }
     }
+});
+/*boton clear list*/
+$('#clearbtn').on('click', function() {
+    $("ul").empty();
 });
